c02381063da10564ecc518a4ec00e9a17eb60244
Manoj <manoj.delhi24@gmail.com>
2017-09-30 16:18:49 +0530

    Controlled field created in sidebar, that will get its value from state.

--- src/containers/sidebar.js
+++ src/containers/sidebar.js
@@ -2,10 +2,25 @@ import React from 'react';
 
 class Sidebar extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a 5 day forecast in your favorite cities. "
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
