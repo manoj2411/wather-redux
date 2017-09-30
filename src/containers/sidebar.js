f59281b9e38f0554e987a7aa5e2e1e459d0fe520
Manoj <manoj.delhi24@gmail.com>
2017-09-30 13:03:29 +0530

    searchbar container created

---
+++ src/containers/sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+
+  render() {
+    return (
+      <form className="input-group">
+        <input />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
+
+export default Sidebar;
