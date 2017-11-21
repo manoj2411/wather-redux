f4bd64251c5384e73166bc5ced5c78f32d207498
Manoj <manoj.delhi24@gmail.com>
2017-11-21 05:39:49 +0530

    weather list for cities boilerplate added

---
+++ src/containers/weather_list.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+class WeatherList extends React.Component {
+
+  renderCityWeather(cityData) {
+    return (
+      <tr key={cityData.city.id}>
+        <td>{cityData.city.name}</td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.props.weather.map(this.renderCityWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {weather: state.weather}
+}
+
+export default connect(mapStateToProps, null)(WeatherList);
