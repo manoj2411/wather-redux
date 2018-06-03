66c9be19c2ee50864950e647cc85c692c6fd7372
Manoj <manoj.delhi24@gmail.com>
2018-06-03 19:24:36 +0530

    App component converted to functional component from class component

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
 
-export default class App extends Component {
-  render() {
+const App = () => {
     return (
       <div>
         <SearchBar />
         <WeatherList />
       </div>
     );
-  }
 }
+
+export default App;
